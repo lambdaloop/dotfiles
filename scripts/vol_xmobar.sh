47c50ca65e7939e4c1f2b97bbb80f39ed054f4de
Pierre Karashchuk <krchtchk@gmail.com>
2018-07-28 04:26:37 -0700

    remove old comments

--- scripts/vol_xmobar.sh
+++ scripts/vol_xmobar.sh
@@ -2,12 +2,6 @@
 
 state=`amixer -c 1 get Master | grep -Eo '\\[(on|off)\\]'`
 
-# if [ $state = '[on]' ]; then
-#     icon='<icon=/home/pierre/.xmonad/icons/sm4tik/xbm/spkr_01.xbm/>'
-# else
-#     icon='<icon=/home/pierre/.xmonad/icons/sm4tik/xbm/spkr_02.xbm/>'
-# fi
-
 if [ $state = '[on]' ]; then
     icon='<fn=1></fn>'
 else
