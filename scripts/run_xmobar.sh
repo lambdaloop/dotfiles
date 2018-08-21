98afa4831992c73ce3895e55e5eb86821d0fa0ed
Pierre Karashchuk <krchtchk@gmail.com>
2018-08-20 22:03:23 -0700

    handle large screen better, and use mopidy instead of mpd

--- scripts/run_xmobar.sh
+++ scripts/run_xmobar.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+screenWidth=`xrandr | sed -rn 's/.*current\s+([0-9]+).*$/\1/p'`
+
+#outputs different stuff depending on large or small screen
+function two-modes
+{
+    if [ "$screenWidth" = '2560' ]; then
+        echo "$1" #left screen
+        #echo "left"
+    else
+        # echo "two"
+        echo "$2" #two screens
+    fi
+}
+
 cd /tmp
 bash ~/scripts/reset_empty_pipe.sh &
-xmobar ~/.xmonad/xmobar/xmobarrc_1 
+
+fname=`two-modes xmobarrc_1 xmobarrc_2`
+xmobar ~/.xmonad/xmobar/$fname 
+
+
+
--- scripts/twoScreenStuff.sh
+++ scripts/twoScreenStuff.sh
@@ -31,14 +31,15 @@ function three-modes-eval
 
 bash ~/scripts/changeBackground.sh &
 
-trayerOpts="--edge top --align right --SetDockType true --SetPartialStrut true --expand true --height 42 --transparent true --tint 0x2D3743 --alpha 0 --padding 0 --widthtype pixel"
+trayerOpts="--edge top --align right --SetDockType true --SetPartialStrut true --expand true --height 42 --transparent true --tint 0x102235 --alpha 0 --padding 0 --widthtype pixel"
 
-percent=`three-modes 16 8`
+# percent=`three-modes 16 16`
 
 # width=`three-modes 2560 1920 1920`
 # trayerWidth=`echo "scale=4; $percent/100 * $width" | bc` #find trayer width, in pixels
 # trayerWidth=`printf %0.f $trayerWidth` #round up trayer width
-trayerWidth=`three-modes 205 308`
+# trayerWidth=`three-modes 205 308`
+trayerWidth=`three-modes 205 205`
 
 echo $trayerWidth
 killall trayer
