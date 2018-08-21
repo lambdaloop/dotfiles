324182876138b2376b4c91d702e78d71eb055634
Pierre Karashchuk <krchtchk@gmail.com>
2018-08-20 22:04:10 -0700

    lucid toolkit, xft for emacs

--- scripts/update_emacs.sh
+++ scripts/update_emacs.sh
@@ -3,7 +3,7 @@
 cd ~/builds/emacs
 git pull origin master
 export PKG_CONFIG_PATH=/usr/lib/imagemagick6/pkgconfig
-./configure --with-imagemagick
+./configure --with-imagemagick --with-x-toolkit=lucid --with-xft
 make -j4
 sudo make install
 
