3edc6b2ed4bc9ec58223e470766116f562b2032a
Pierre Karashchuk <krchtchk@gmail.com>
2018-09-09 19:26:59 -0700

    rofi theme

--- scripts/pick_program.sh
+++ scripts/pick_program.sh
@@ -7,4 +7,4 @@ PATH=/home/pierre/bin:/home/pierre/.cabal/bin:$PATH
 # /home/pierre/.cabal/bin/yeganesh -x -- -i -nb '#2d2d2d' -sb '#37526e'  -b -fn "Monospace-9" -l 10 | bash
 # rofi -x -- -i -nb '#2d2d2d' -sb '#37526e'  -b -fn "Monospace-9" -l 10 | bash
 
-rofi -show run -font "Dejavu Sans Mono 20"
+rofi -show run -theme ~/.config/rofi/onedark.rasi
