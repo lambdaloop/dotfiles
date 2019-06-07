efc88f24c5faa2d4c4290a67800767ccdd232f84
Pierre Karashchuk <krchtchk@gmail.com>
2019-06-07 01:52:47 -0700

    refactor variables into env

---
+++ scripts/setenv.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export PATH=/home/pierre/.cabal/bin:$PATH
+export PATH=/home/pierre/bin:$PATH
+export PATH=/home/pierre/.local/bin:$PATH
+export PATH=/opt/android-sdk/platform-tools:$PATH
+
+## HiDPI (Retina) settings
+export GDK_SCALE=0.5
+export GDK_DPI_SCALE=1
+export ELM_SCALE=1.5
+
+# export QT_DEVICE_PIXEL_RATIO=2
+export QT_AUTO_SCREEN_SCALE_FACTOR=2
+
+export BROWSER=firefox;
