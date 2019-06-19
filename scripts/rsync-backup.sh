2034f4103dcc479548efb02b50a8ff30f8312141
Pierre Karashchuk <krchtchk@gmail.com>
2019-06-19 03:03:29 -0700

    new backup script

--- scripts/rsync-backup.sh
+++ scripts/rsync-backup.sh
@@ -2,10 +2,13 @@
 
 export BORG_PASSPHRASE=`pass online/rsync.net-backup`
 
-folders=(~/cs ~/research ~/learning ~/Dropbox)
-folders_unlink=`for folder in ${folders[*]}; do readlink $folder; done | tr '\n' ' '`
+# folders=(~/cs ~/research ~/learning ~/Dropbox)
+# folders_unlink=`for folder in ${folders[*]}; do readlink $folder; done | tr '\n' ' '`
 
 borg create -C lz4 -v -p --stats --remote-path=borg1 \
+     --exclude-from ~/scripts/rsync_backup_exclude.txt \
+     --exclude-if-present .nobak \
      rsync:backup::jellyfish-{user}-{now:%Y-%m-%dT%H-%M} \
-     $folders_unlink
+     /jellyfish
+     # $folders_unlink
 #~/Camera
